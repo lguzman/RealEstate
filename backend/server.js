9f313d4e60075ba8a7d583ab13397208521f3e90
Luis Guzman <luismguzman@gmail.com>
2025-02-10 20:31:07 -0400

    Added shared navigation bar to all pages

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+
+// Set the home page as the default route BEFORE serving static files
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "home.html"));
+});
 
 // Serve static files (HTML, CSS, JS)
 app.use(express.static("public"));
